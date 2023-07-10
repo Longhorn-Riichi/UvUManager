69ed62d0acd2ecf06f1e8e8fbc7cf37b8a2db345
Peter Gao <peter1357908@hotmail.com>
2023-07-10 18:00:30 -0500

    can now receive mahjong soul notifications consistently

--- ext/UvUManager/cog.py
+++ ext/UvUManager/cog.py
@@ -6,25 +6,33 @@ import json
 
 from modules.mahjongsoul.contest_manager import ContestManager
 
+EXTENSION_NAME = "UvUManager" # must be the same as class name...
+
 class UvUManager(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
         json_config_path = join(dirname(__file__), "config.json")
         with open(json_config_path, "r") as f:
-            self.json_config = json.loads(f.read())
-        
-        # TODO: failure to get bot channel?
-        self.bot_channel = bot.get_channel(self.json_config["bot_channel_id"])
+            self.json_config = json.load(f)
+        self.bot_channel = None # fetched in `self.async_setup()`
         self.manager = ContestManager(self.json_config["contest_unique_id"])
 
-    # to be called in `setup()`:
-    # connect, login, and subscribe to events...
     async def async_setup(self):
+        """
+        to be called in `setup()`:
+        1. fetch the channel specified in JSON
+        2. connect and login to Mahjong Soul...
+        3. subscribe to relevant events
+        """
+        
+        # note that `bot.get_channel()` doesn't work because at this point
+        # the bot has not cached the channel yet...
+        self.bot_channel = await self.bot.fetch_channel(self.json_config["bot_channel_id"])
         await self.manager.connect_and_login()
         await self.manager.subscribe('NotifyContestGameStart', self.on_NotifyContestGameStart)
         await self.manager.subscribe('NotifyContestGameEnd', self.on_NotifyContestGameEnd)
-    
+
     @app_commands.command(name="terminate", description="Terminate the game of the specified player.")
     async def terminate(self, interaction, nickname: str):
         """
@@ -51,13 +59,12 @@ class UvUManager(commands.Cog):
     
     async def on_NotifyContestGameStart(self, _, msg):
         nicknames = ' | '.join([p.nickname for p in msg.game_info.players])
-        self.bot_channel.send(f"UvU game started! Players: {nicknames}.")
+        await self.bot_channel.send(f"UvU game started! Players: {nicknames}.")
     
     async def on_NotifyContestGameEnd(self, _, msg):
         # It takes some time for the results to register into the log
         await asyncio.sleep(3)
 
-        # TODO: potential Error? Requires login and subscribing?
         record = await self.manager.locate_completed_game(msg.game_uuid)
 
         response = None
@@ -77,8 +84,14 @@ class UvUManager(commands.Cog):
 
         await self.bot_channel.send(response)
 
-async def setup(bot):
+async def setup(bot: commands.Bot):
     instance = UvUManager(bot)
     asyncio.create_task(instance.async_setup())
     await bot.add_cog(instance)
-    print(f'Extension `UvUManager` setup finished')
+    print(f'Extension `{EXTENSION_NAME}` is being loaded')
+
+# `teardown()` currently doesn't get called for some reason...
+# async def teardown(bot: commands.Bot):
+#     instance: UvUManager = bot.get_cog(EXTENSION_NAME)
+#     await instance.manager.close()
+#     print(f'Extension `{EXTENSION_NAME}` is being unloaded')
