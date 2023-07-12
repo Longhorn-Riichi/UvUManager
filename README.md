3009045185a383af5151a2a0be062f80ddb3d019
Peter Gao <peter1357908@hotmail.com>
2023-07-12 09:14:58 -0500

    Added affiliation to Register and removed redundancies

--- README.md
+++ README.md
@@ -11,16 +11,9 @@ A bot for Longhorn Riichi. Helps record online club games and provides utilities
         pipenv shell
         ./start.sh
 
-### recompiling the Mahjong Soul protobuf files
-If you encounter issues with Mahjong Soul protobuf files in the `ms_api` package, try recompiling them with the included `liqi_admin.json`:
-1. install the latest Protobuf **Compiler** (>=3.20). `apt` may have an oudated version so you have to get the right binary from [their GitHub](https://github.com/protocolbuffers/protobuf/releases) and put it in `/usr/local/bin`
-1. `cd` into [modules/mahjongsoul/protocol](./modules/mahjongsoul/protocol) and run `./update_proto_liqi_admin.sh`
-
-### updating the Mahjong Soul protocol file
-You can obtain the latest `liqi_admin.json` by extracting the `nested` JS object in the obfuscated `app.xxxx.js` from the tournament manager site like [this](https://github.com/MahjongRepository/mahjong_soul_api/issues/14#issuecomment-1624183351).
-
 # References
 
 - [Riichi Nomi's Ronnie bot](https://github.com/RiichiNomi/ronnie)
+- [mjsou.py](https://github.com/RiichiNomi/mjsoul.py)
 - [ms_api: Mahjong Soul API in Python](https://github.com/MahjongRepository/mahjong_soul_api/tree/master)
 
--- ext/UvUManager/cog.py
+++ ext/UvUManager/cog.py
@@ -8,15 +8,19 @@ from modules.mahjongsoul.contest_manager import ContestManager
 
 EXTENSION_NAME = "UvUManager" # must be the same as class name...
 
+json_config_path = join(dirname(__file__), "config.json")
+with open(json_config_path, "r") as f:
+    json_config = json.load(f)
+CONTEST_UNIQUE_ID = json_config["contest_unique_id"]
+GUILD_ID = json_config["guild_id"]
+BOT_CHANNEL_ID = json_config["bot_channel_id"]
+
+
 class UvUManager(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-
-        json_config_path = join(dirname(__file__), "config.json")
-        with open(json_config_path, "r") as f:
-            self.json_config = json.load(f)
         self.bot_channel = None # fetched in `self.async_setup()`
-        self.manager = ContestManager(self.json_config["contest_unique_id"])
+        self.manager = ContestManager(CONTEST_UNIQUE_ID)
 
     async def async_setup(self):
         """
@@ -28,7 +32,7 @@ class UvUManager(commands.Cog):
         
         # note that `bot.get_channel()` doesn't work because at this point
         # the bot has not cached the channel yet...
-        self.bot_channel = await self.bot.fetch_channel(self.json_config["bot_channel_id"])
+        self.bot_channel = await self.bot.fetch_channel(BOT_CHANNEL_ID)
         await self.manager.connect_and_login()
         await self.manager.subscribe('NotifyContestGameStart', self.on_NotifyContestGameStart)
         await self.manager.subscribe('NotifyContestGameEnd', self.on_NotifyContestGameEnd)
@@ -41,6 +45,7 @@ class UvUManager(commands.Cog):
 
     @app_commands.command(name="terminate", description="Terminate the game of the specified player.")
     @app_commands.describe(nickname="Specify the nickname of a player that's in the game you want to terminate.")
+    @app_commands.guilds(GUILD_ID)
     async def terminate(self, interaction: Interaction, nickname: str):
         """
         TODO: ensure that it's either the caller terminating their own game or it's the admin terminating any game; allow terminating own game without nickname input -- requires looking up mahjong soul ID
@@ -65,11 +70,20 @@ class UvUManager(commands.Cog):
         )
     
     @app_commands.command(name="register", description="Register yourself with your Mahjong Soul friend ID.")
-    @app_commands.describe(friend_id="Find your friend ID in the Friends tab; this is separate from your username.")
-    async def register(self, interaction: Interaction, friend_id: int):
+    @app_commands.choices(affiliation=[
+        app_commands.Choice(name="UT Austin", value="UT Austin"),
+        app_commands.Choice(name="UT Dallas", value="UT Dallas")
+    ])
+    @app_commands.describe(
+        friend_id="Find your friend ID in the Friends tab; this is separate from your username.",
+        affiliation="Which club you represent: Austin? Dallas?"
+    )
+    @app_commands.guilds(GUILD_ID)
+    async def register(self, interaction: Interaction, friend_id: int, affiliation: app_commands.Choice[str]):
         """
         here we use Mahjong Soul ID as the unique identifier, since the
         tournament will be held over Mahjong Soul anyway.
+        TODO: allow registration only for a select Discord role
         """
         res = await self.manager.call(
             "searchAccountByEid",
@@ -77,21 +91,25 @@ class UvUManager(commands.Cog):
         )
 
         if res.search_result:
-            nickname = res.search_result[0].nickname
+            mahjongsoul_nickname = res.search_result[0].nickname
             # NOTE: account ID is different from friend ID!!!
-            account_id = res.search_result[0].account_id
+            mahjongsoul_account_id = res.search_result[0].account_id
             discord_name = interaction.user.name
 
-            # TODO: record account ID, mahjong soul nickname, and discord name to Google Sheets here
+            # TODO: record `mahjongsoul_account_id`, `mahjongsoul_nickname`, `discord_name`, and `affiliation.value` to Google Sheets here.
+            # NOTE: if a Discord user tries to register again, it should override the existing entry that has the same Discord name.
 
             await interaction.response.send_message(
-                content=f"Mahjong Soul user \"{nickname}\" has been registered with Discord user \"{discord_name}\".",
+                content=f"\"{discord_name}\" from {affiliation.value} has registered their Mahjong Soul account \"{mahjongsoul_nickname}\".",
             )
         else:
             await interaction.response.send_message(
                 content=f"Couldn't find Mahjong Soul account for this friend ID: {friend_id}",
             )
 
+    # @app_commands.command(name="start", description="Propose to start a game.")
+    # @app_commands.guilds(GUILD_ID)
+    # async def start(self, interaction: Interaction):
 
     """
     =====================================================
